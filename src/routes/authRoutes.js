0333a507270b9b2085f0f43233ce1363c91aba1f
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-05-21 23:01:18 -0500

    Agregué rutas para el cambio de contraseña y funciones al mailer

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const authRouter = Router();
 
-const { signIn, signUp, editAuthById, getAuth, putAuth } = require("../controllers/authController");
+const { signIn, signUp, forgotPassword, editAuthById, getAuth, putAuth, resetPassword } = require("../controllers/authController");
 const { requireAuth } = require("../middlewares/auth");
 
 authRouter.post("/signin", signIn);
 authRouter.post("/signup", signUp);
+authRouter.post("/forgot-password", forgotPassword);
+authRouter.post("/reset-password",  resetPassword);
 authRouter.put("/", requireAuth, editAuthById);
 authRouter.get("/", getAuth);
 authRouter.put("/:id", putAuth);
