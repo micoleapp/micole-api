592f376aab4bb96bf77dd77577ee81d280195d31
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-05-26 14:59:08 -0500

    Primeras rutas añadidas a la documentación

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,93 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const userRouter = Router();
 
-const { getUsers, getUserById, deleteUserById } = require("../controllers/userController");
-/* const { requireAuth } = require("../middlewares/auth"); */
+const {
+  getUsers,
+  getUserById,
+  deleteUserById,
+} = require('../controllers/userController');
 
-userRouter.get("/", getUsers);
-userRouter.get("/:idUser", getUserById);
-userRouter.delete("/:idUser", deleteUserById);
+/**
+ * @swagger
+ * tags:
+ *   name: Usuarios
+ *   description: Gestionar usuarios
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Obtener todos los usuarios
+ *     description: Obtener la lista de todos los usuarios registrados
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - name: limit
+ *         in: query
+ *         description: Número máximo de usuarios a devolver por página
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *       - name: page
+ *         in: query
+ *         description: Número de página
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *     responses:
+ *       200:
+ *         description: Éxito. Devuelve la lista de usuarios.
+ *       404:
+ *         description: No encontrado. No se encontraron usuarios en la DB.
+ *       500:
+ *         description: Error del servidor. No se pudo obtener la lista de usuarios.
+ */
+userRouter.get('/', getUsers);
+
+/**
+ * @swagger
+ * /users/{idUser}:
+ *   get:
+ *     summary: Obtener un usuario por ID
+ *     description: Obtener un usuario específico por su ID
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: idUser
+ *         description: ID del usuario a obtener
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Éxito. Devuelve el usuario solicitado.
+ *       404:
+ *         description: No encontrado. No se encontró el usuario con el ID especificado.
+ */
+userRouter.get('/:idUser', getUserById);
+
+/**
+ * @swagger
+ * /users/{idUser}:
+ *   delete:
+ *     summary: Eliminar un usuario por ID
+ *     description: Eliminar un usuario específico por su ID
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: idUser
+ *         description: ID del usuario a eliminar
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Éxito. El usuario ha sido eliminado correctamente.
+ *       404:
+ *         description: No encontrado. No se encontró el usuario con el ID especificado.
+ */
+userRouter.delete('/:idUser', deleteUserById);
 
 module.exports = userRouter;
